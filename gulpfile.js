87e67476197a26f9b2ab894fc4155d085ade7c39
高橋侑子 <>
2018-12-05 20:39:31 +0900

    news文字色変更まで

--- gulpfile.js
+++ gulpfile.js
@@ -1,122 +1,48 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var header = require('gulp-header');
-var cleanCSS = require('gulp-clean-css');
-var rename = require("gulp-rename");
-var uglify = require('gulp-uglify');
-var autoprefixer = require('gulp-autoprefixer');
-var pkg = require('./package.json');
-var browserSync = require('browser-sync').create();
+'use strict';
 
-// Set the banner content
-var banner = ['/*!\n',
-  ' * Start Bootstrap - <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n',
-  ' * Copyright 2013-' + (new Date()).getFullYear(), ' <%= pkg.author %>\n',
-  ' * Licensed under <%= pkg.license %> (https://github.com/BlackrockDigital/<%= pkg.name %>/blob/master/LICENSE)\n',
-  ' */\n',
-  '\n'
-].join('');
+var gulp    = require('gulp'),
+    sass    = require('gulp-sass'),
+    glob    = require('gulp-sass-glob'), // sassのimportでワイルトカードを利用可能に
+    maps    = require('gulp-sourcemaps'),
+    prefix  = require('gulp-autoprefixer'),
+    plumber = require('gulp-plumber'),
+    notify  = require('gulp-notify'),
+    bs      = require('browser-sync').create();
 
-// Copy third party libraries from /node_modules into /vendor
-gulp.task('vendor', function() {
-
-  // Bootstrap
-  gulp.src([
-      './node_modules/bootstrap/dist/**/*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-grid*',
-      '!./node_modules/bootstrap/dist/css/bootstrap-reboot*'
-    ])
-    .pipe(gulp.dest('./vendor/bootstrap'))
-
-  // Font Awesome 5
-  gulp.src([
-      './node_modules/@fortawesome/**/*'
-    ])
-    .pipe(gulp.dest('./vendor'))
-
-  // jQuery
-  gulp.src([
-      './node_modules/jquery/dist/*',
-      '!./node_modules/jquery/dist/core.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery'))
-
-  // jQuery Easing
-  gulp.src([
-      './node_modules/jquery.easing/*.js'
-    ])
-    .pipe(gulp.dest('./vendor/jquery-easing'))
-
-});
-
-// Compile SCSS
-gulp.task('css:compile', function() {
-  return gulp.src('./scss/**/*.scss')
-    .pipe(sass.sync({
-      outputStyle: 'expanded'
-    }).on('error', sass.logError))
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions'],
+// gulp-sassの設定
+gulp.task('sass', function() {
+  gulp.src('./scss/style.scss')
+    .pipe(plumber({errorHandler: notify.onError('<%= error.message %>')}))
+    .pipe(maps.init())
+    .pipe(glob({ignorePaths: ['*node_modules*']}))
+    .pipe(sass({outputStyle: 'expanded'}))
+    // ベンダープレフィックス自動付加
+    .pipe(prefix({
+      browsers: ['last 2 version', 'iOS >= 8', 'Android >= 4.1'],
       cascade: false
     }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
+    // Sassの編集時はリロードせずにブラウザに反映
+    .pipe(maps.write('./'))
     .pipe(gulp.dest('./css'))
+    .pipe(bs.stream());
 });
 
-// Minify CSS
-gulp.task('css:minify', ['css:compile'], function() {
-  return gulp.src([
-      './css/*.css',
-      '!./css/*.min.css'
-    ])
-    .pipe(cleanCSS())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(gulp.dest('./css'))
-    .pipe(browserSync.stream());
-});
-
-// CSS
-gulp.task('css', ['css:compile', 'css:minify']);
-
-// Minify JavaScript
-gulp.task('js:minify', function() {
-  return gulp.src([
-      './js/*.js',
-      '!./js/*.min.js'
-    ])
-    .pipe(uglify())
-    .pipe(rename({
-      suffix: '.min'
-    }))
-    .pipe(header(banner, {
-      pkg: pkg
-    }))
-    .pipe(gulp.dest('./js'))
-    .pipe(browserSync.stream());
-});
-
-// JS
-gulp.task('js', ['js:minify']);
-
-// Default task
-gulp.task('default', ['css', 'js', 'vendor']);
-
-// Configure the browserSync task
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
+// BrowserSyncの設定
+// ブラウザ画面への通知を無効化
+gulp.task('sync', function() {
+  bs.init({
+    proxy: "localhost/basskie/",
+    notify: false
   });
 });
 
-// Dev task
-gulp.task('dev', ['css', 'js', 'browserSync'], function() {
-  gulp.watch('./scss/*.scss', ['css']);
-  gulp.watch('./js/*.js', ['js']);
-  gulp.watch('./*.html', browserSync.reload);
+gulp.task('reload', function() {
+  bs.reload();
 });
+
+gulp.task('watch', function() {
+  gulp.watch(['./scss/style.scss'], ['sass']);
+  gulp.watch(['./js/**/*.js','./*.php'], ['reload']);
+});
+
+gulp.task('default', ['sass', 'sync', 'reload', 'watch']);
